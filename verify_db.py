714a9c989dad7662fc634476569bb7289a9654be
abdalluhmohy <abdalluh.albaz@hotmail.com>
2026-09-19 20:10:43 +0200

    Delete verify_db.py

--- verify_db.py
+++
@@ -1,6 +0,0 @@
-import app
-
-app.initialize_database()
-connection = app.connect_db()
-print([row[0] for row in connection.execute("SELECT name FROM sqlite_master WHERE type = 'table'").fetchall()])
-connection.close()
